52192460cc70d5661362da0c982c68522f257436
elraftuur <restutoi18@gmail.com>
2026-07-31 17:40:36 +0700

    use

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,23 +1,10 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
+console.log("API URL =", process.env.NEXT_PUBLIC_API_URL);
+
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
-
-api.interceptors.request.use((config) => {
-  const token = Cookies.get("token");
-
-  console.log("REQUEST URL:", config.url);
-  console.log("TOKEN:", token);
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  console.log("AUTH:", config.headers.Authorization);
-
-  return config;
 });
 
 export default api;
